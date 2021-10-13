e162db80b75b211e519e8e6a8316620f32a4c6e1
Donald Le <ledinhcuong99@gmail.com>
2021-10-13 11:18:07 +0700

    Refactor code

--- src/main/java/com/cuongld/service/CreateRequestService.java
+++ src/main/java/com/cuongld/service/CreateRequestService.java
@@ -2,16 +2,13 @@ package com.cuongld.service;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.datamodeling.*;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.cuongld.model.Product;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.log4j.Logger;
-
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
+
 
 
 public class CreateRequestService {
@@ -40,58 +37,10 @@ public class CreateRequestService {
                 .build());
     }
 
-    public Boolean ifTableExists() {
-        return this.dynamoDB.describeTable(PRODUCTS_TABLE_NAME).getTable().getTableStatus().equals("ACTIVE");
-    }
-
-    public List<Product> list() throws IOException {
-        DynamoDBScanExpression scanExp = new DynamoDBScanExpression();
-        List<Product> results = this.dynamoDBMapper.scan(Product.class, scanExp);
-        for (Product p : results) {
-            logger.info("Products - list(): " + p.toString());
-        }
-        return results;
-    }
-
-    public Product get(String id) throws IOException {
-        Product product = null;
-
-        HashMap<String, AttributeValue> av = new HashMap<String, AttributeValue>();
-        av.put(":v1", new AttributeValue().withS(id));
-
-        DynamoDBQueryExpression<Product> queryExp = new DynamoDBQueryExpression<Product>()
-                .withKeyConditionExpression("id = :v1")
-                .withExpressionAttributeValues(av);
-
-        PaginatedQueryList<Product> result = this.dynamoDBMapper.query(Product.class, queryExp);
-        if (result.size() > 0) {
-            product = result.get(0);
-            logger.info("Products - get(): product - " + product.toString());
-        } else {
-            logger.info("Products - get(): product - Not Found.");
-        }
-        return product;
-    }
 
     public void save(Product product) throws IOException {
         logger.info("Products - save(): " + product.toString());
         this.dynamoDBMapper.save(product);
     }
-
-    public Boolean delete(String id) throws IOException {
-        Product product = null;
-
-        // get product if exists
-        product = get(id);
-        if (product != null) {
-            logger.info("Products - delete(): " + product.toString());
-            this.dynamoDBMapper.delete(product);
-        } else {
-            logger.info("Products - delete(): product - does not exist.");
-            return false;
-        }
-        return true;
-    }
-
 
 }
